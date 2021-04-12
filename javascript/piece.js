9237d4ea95ae9c4a7273f0ac3a1521124c395def
Akshay Agarwal <mathgeek145@gmail.com>
2021-04-12 21:31:53 +0800

    javascript-update

--- javascript/piece.js
+++ javascript/piece.js
@@ -1,18 +1,54 @@
-//24 elements
+
+class Piece{
+
+
+    constructor(number, square){
+        this.number = number; 
+        this.square = square; 
+    }
+
+
+    
+    move = (event) =>{
+        console.log(event.clientX, event.clientY); 
+    }
+
+
+    listenEvent = () =>{
+        let piecePhoto = document.getElementById(`piece-images${this.number}`); 
+        console.log(piecePhoto); 
+        piecePhoto.addEventListener('click', this.move); 
+    }
+
+    
+
+
+
+}
+
+let Test = new Piece(16, [2,2]); 
+Test.listenEvent(); 
+
+
+
+
+
+
+
+//desktop version -> works on 13 inch, 15 inch, and 24 inch screens: 
 
 let pieceImages = []; 
 let width = window.innerWidth; 
 let height = window.innerHeight; 
+let numberOfPieces = []; //insert array of the number of duplicates each piece in the game begins with
 
 
-document.documentElement.style.setProperty('--pieceSize', `${width * 0.08}px`); 
+document.documentElement.style.setProperty('--pieceSize', `${width * 0.08*0.85}px`); 
 
-console.log("HEIGHT", height); 
-console.log(width); 
+
 let pieceSize = width * 0.08; 
 let heightPieceSize = height * 0.21; 
 
-console.log(pieceSize);
 
 for(let i = 1; i<=24; i++){
     pieceImages.push(document.getElementById(`piece-images${i}`)); 
